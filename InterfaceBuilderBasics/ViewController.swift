359723c4527c46c3af1fd41464f6da75d8d7ed7b
Jacquita Adams <adamslaquita24@gmail.com>
2020-07-23 19:52:28 -0500

    Initial Commit

---
+++ InterfaceBuilderBasics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InterfaceBuilderBasics
+//
+//  Created by MXC Swift on 7/23/20.
+//  Copyright © 2020 MXC Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
